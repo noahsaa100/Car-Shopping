5d8af82b281c2e53783f68d90f2a4a06d5f465ed
noahsaa <c2042391@newcastle.ac.uk>
2024-03-26 00:38:44 +0000

    Added garages owned

--- src/Objects/User.java
+++ src/Objects/User.java
@@ -1,10 +1,32 @@
 package Objects;
 
 import java.text.DecimalFormat;
+import java.util.List;
 
 public class User {
     private String Name;
     private double Balance;
+    private boolean ownsGarage;
+    private List<Garage> garagesOwned;
+
+    public boolean isOwnsGarage() {
+        if(!garagesOwned.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public void setOwnsGarage(boolean ownsGarage) {
+        this.ownsGarage = ownsGarage;
+    }
+
+    public List<Garage> getGaragesOwned() {
+        return garagesOwned;
+    }
+
+    public void setGaragesOwned(List<Garage> garagesOwned) {
+        this.garagesOwned = garagesOwned;
+    }
 
     public User(String name, double balance) {
         this.Name = name;
